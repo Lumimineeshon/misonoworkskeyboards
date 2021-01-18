52fdc9a2cae0b2192f6feffa868881bb27d6b2bd
Lumimineeshon <chiajinwen@gmail.com>
2021-01-18 23:29:26 +0800

    mirubere keymap v1
    mirubere's keymap for the karina
    differences from default:
    swapped alt and ctrl
    tapping (super) has `~ coded to it
    tapping (meta) has 2@ coded to it
    layer (meta) enables brightness changes with right knob
    removed left down right arrow keys from layer (super)

--- karina/keymaps/mirubere/keymap.c
+++ karina/keymaps/mirubere/keymap.c
@@ -1,3 +1,20 @@
+/*
+Copyright 2020 MisonoWorks
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include QMK_KEYBOARD_H
 
 enum layers {
@@ -12,12 +29,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, 
 		KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_ENT,
 		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_MUTE, 
-		KC_LCTL, LM(SUPER, KC_TILD), KC_SPC, KC_BSPC, LM(META, KC_AT), KC_LALT),
+		KC_LCTL, LT(SUPER, KC_TILD), KC_SPC, KC_BSPC, LT(META, KC_AT), KC_LALT),
 
     [SUPER] = LAYOUT(
         KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, 
 		KC_MINUS, KC_EQUAL, KC_GRAVE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_QUOT, KC_SCLN, 
-		KC_LSFT, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SLSH, KC_BSLS, KC_TRNS,
+		KC_LSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SLSH, KC_BSLS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, KC_GRV, KC_TRNS),
 		
 	[META] = LAYOUT(
@@ -36,6 +53,15 @@ void encoder_update_user(uint8_t index, bool clockwise) {
             tap_code(KC_PGDOWN);
         }
     }
+    else if(IS_LAYER_ON(META)){
+	if(index == 1){
+	    if (clockwise) {
+                rgblight_increase_val();
+            } else {
+                rgblight_decrease_val();
+            }
+	}
+    }
     else if (index == 1) {
         if (clockwise) {
             tap_code(KC_VOLU);
@@ -43,14 +69,6 @@ void encoder_update_user(uint8_t index, bool clockwise) {
             tap_code(KC_VOLD);
         }
     }
-    else if(IS_LAYER_ON(META){
-	if(index == 1){
-	    if (clockwise) {
-                tap_code(RGB_VAI);
-            } else {
-                tap_code(RGB_VAD);
-            }
-	}
-    }
+    
 }
 
